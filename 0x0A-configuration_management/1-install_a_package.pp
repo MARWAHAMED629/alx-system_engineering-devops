bf6c8013ee28253f2e270f8d8cfd8a90e5f1c8ec
MARWAHAMED629 <mmero385@gmail.com>
2024-02-23 09:44:16 -0800

    install a package []

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,18 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
-        ensure   => '2.1.0',
-        provider => 'pip3'
+# Install flask package version 2.1.0 with pip3
+
+class { 'python::pip': }
+
+package { 'Flask':
+  ensure => 'latest',
+  provider => 'pipx',
+  install_options => [ '--index-url https://pypi.org/simple' ], # Optional: Use PyPI index
+  require => Class['python::pip']
+}
+
+# Ensure flask version is 2.1.0
+exec { 'check_flask_version':
+  command => 'pip3 show flask | grep Version | cut -d" " -f2',
+  unless => '/usr/bin/pip3 show flask | grep Version | grep "2.1.0"',
+  notify => Package['Flask'],
+  failonerror => true
 }
