e3f67212042f54f4f94a4ae484185c830a560079
MARWAHAMED629 <mmero385@gmail.com>
2024-02-23 16:24:49 -0800

    update file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,12 @@
-#!/usr/bin/puppet
+#!/usr/bin/pup
 # Install Flask version 2.1.0 using pip3
 
-package { 'flask':
+package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
 
-package { 'werkzeug':
+package {'werkzeug':
   ensure   => '2.1.1',
-  provider   => 'pip3',
+  provider => 'pip3',
 }
