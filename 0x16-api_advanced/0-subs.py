c878726528b35e94f6ddce6c9349086408814380
root <mmero385@gmail.com>
2024-05-07 15:02:52 -0700

    api Advanced

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Reddit API subscribers
+"""
+
+from requests import get
+
+
+def number_of_subscribers(subreddit):
+    """Get Number of subscribers"""
+
+    subreddit_info = get(
+        "https://www.reddit.com/r/{:s}/about.json".format(subreddit),
+        headers={"User-Agent": "Custom-User-Agent"},
+        allow_redirects=False)
+
+    return (0 if subreddit_info.status_code >= 300 else
+            subreddit_info.json().get('data').get('subscribers'))
